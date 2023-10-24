ce1547bbc551eeeae026b4cf60e1cbc5f4005b07
HakanKara75 <hakanbatirhan@gmail.com>
2023-10-24 14:56:56 +0300

    v

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -459,7 +459,15 @@ public class ReusableMethods {
     public static By locateElementByText(String text){
         return AppiumBy.androidUIAutomator("new UiSelector().text(\""+text+"\")");
     }
+    public void tapOnWithPoint(AppiumDriver driver, int x, int y) {
 
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence tap = new Sequence(finger, 1);
+        tap.addAction(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), x, y));
+        tap.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        tap.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        driver.perform(Arrays.asList(tap));
+    }
 }
 
 
